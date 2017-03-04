2d87bd4bb9d90bacde221222b7606def0066d62b
dzejkob23 <j.zika23@gmail.com>
2017-03-04 02:16:51 +0100

    Clean project

---
+++ BluetoothScanner/app/src/main/java/com/example/dzejkob23/bluetoothscaner/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.dzejkob23.bluetoothscaner;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final BroadcastReceiver bReciever = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                String name = device.getName();
+                String address = device.getAddress();
+
+                TextView tv = (TextView) findViewById(R.id.textView);
+                tv.setText(name + "\n" + address + "\n" + tv.getText());
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickScan() {
+        bReciever.goAsync();
+    }
+
+}
