0da0107928ffd8eb54edbe17d0906b411da97aab
Manas7 <hronmar@seznam.cz>
2017-09-19 01:06:39 +0200

    Optimalization for one car. Removed pairing parking places between each other. Added threshold edit text field.

--- BluetoothScanner/app/src/main/java/com/example/aimtechackathon2017/bluetoothscanner/MainActivity.java
+++ BluetoothScanner/app/src/main/java/com/example/aimtechackathon2017/bluetoothscanner/MainActivity.java
@@ -7,15 +7,15 @@ import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -27,6 +27,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,7 +35,8 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity
 {
-    private static int THRESHOLD = 40;
+    private static final int  DEFAULT_THRESHOLD = 60;//40;
+    private int threshold = 60;
     private static int COUNT_MAX = 2;
     private static int COUNT_MIN = 0;
 
@@ -43,6 +45,8 @@ public class MainActivity extends AppCompatActivity
     private Map<String, Integer> positions;
 
     private static final int REQUEST_ENABLE_BT = 1;
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
     private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback() {
         @Override
@@ -56,6 +60,12 @@ public class MainActivity extends AppCompatActivity
                 String mac = device.getAddress().trim().replace(" ",":");
 
                 if(positions.containsKey(mac)) {
+                    byte[] pairedMac = Arrays.copyOfRange(scanRecord, 35, 41);
+
+                    //if place is paired with place
+                    if(positions.containsKey(bytesToHexMac(pairedMac)))
+                        return;
+
                     intensity = scanRecord[scanRecord.length - 20];
                     boolean value = isPositionEmpty(mac, intensity);
 
@@ -76,7 +86,20 @@ public class MainActivity extends AppCompatActivity
                                 JSONObject entity = new JSONObject();
                                 entity.put("available", Boolean.parseBoolean(params[0]));
                                 entity.put("place_mac", params[1]);
+
                                 parent.put(entity);
+
+                                if(!Boolean.parseBoolean(params[0])) {
+                                    for (String place_mac : positions.keySet()) {
+                                        if (!place_mac.equalsIgnoreCase(params[1])) {
+                                            JSONObject anotherEntity = new JSONObject();
+                                            anotherEntity.put("available", true);
+                                            anotherEntity.put("place_mac", place_mac);
+
+                                            parent.put(anotherEntity);
+                                        }
+                                    }
+                                }
 
                                 OutputStream out = conn.getOutputStream();
                                 out.write(parent.toString().getBytes());
@@ -88,7 +111,7 @@ public class MainActivity extends AppCompatActivity
                                 byte [] array = new byte[1024];
 
                                 is.read(array);
-                                String result = new String(array);
+                                //String result = new String(array);
                                 is.close();
                             }
                             catch (MalformedURLException e) {
@@ -121,6 +144,19 @@ public class MainActivity extends AppCompatActivity
     public void startScan(View v)
     {
         TextView myAwesomeTextView = (TextView)findViewById(R.id.activeTV);
+        myAwesomeTextView.setText(R.string.active);
+
+        EditText thresholdEditText = (EditText)findViewById(R.id.thresholdEditText);
+        String threshold = thresholdEditText.getText().toString();
+
+        if (TextUtils.isEmpty(threshold)) {
+            thresholdEditText.setText(Integer.toString(DEFAULT_THRESHOLD));
+            this.threshold = DEFAULT_THRESHOLD;
+        }
+        else {
+            this.threshold = Integer.parseInt(threshold);
+        }
+
         myAwesomeTextView.setText(R.string.active);
 
         if(bAdapter != null)
@@ -173,33 +209,55 @@ public class MainActivity extends AppCompatActivity
     private void initializePosition()
     {
         positions = new HashMap<String, Integer>();
-        positions.put("A0:E6:F8:EB:DB:A6",0);
-        positions.put("A0:E6:F8:EB:DF:A3",0);
-        positions.put("A0:E6:F8:37:6A:34",0);
+        positions.put("A0:E6:F8:EB:C9:13",0);
+        positions.put("A0:E6:F8:EB:C8:E7",0);
+        positions.put("A0:E6:F8:EB:C9:32",0);
     }
 
     private boolean isPositionEmpty(String mac, int intesity)
     {
         int i = (int)positions.get(mac);
 
-        if (THRESHOLD > Math.abs(intesity)){
+        if (threshold > Math.abs(intesity)){
 
             Log.d("PARKING", mac + " " + intesity);
 
-            if(i < COUNT_MAX)
+            return false;
+
+            /*if(i < COUNT_MAX)
             {
                 positions.put(mac, i + 1);
-            }
+            }*/
         }
         else {
             Log.d("FREE", mac + " " + intesity);
-            if(i > COUNT_MIN)
+            /*if(i > COUNT_MIN)
             {
                 positions.put(mac, i - 1);
-            }
+            }*/
+            return true;
         }
 
-        return i == COUNT_MIN;
+        //return i == COUNT_MIN;
     }
 
+    public String bytesToHexMac(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = hexChars.length - 1; i >= 0; i -= 2){
+            sb.append(hexChars[i - 1]);
+            sb.append(hexChars[i]);
+
+            if(i - 1 != 0)
+                sb.append(':');
+        }
+
+        return sb.toString();
+    }
 }
